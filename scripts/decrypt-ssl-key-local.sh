e0c3470d412580436284710e120317be7ae0dd45
Martin Eden <martin.s.eden@pm.me>
2018-07-20 11:32:12 +0100

    Re-encrypt using new symmetric key

---
+++ scripts/decrypt-ssl-key-local.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+HERE=$(dirname $0)
+cd $HERE/../ssl-key
+
+FILE_ORIGINAL=ssl_private_key
+FILE_ENC=ssl_private_key.enc
+FILE_CLEAR=ssl_private_key.copy
+
+echo "Enter symmetric key: "
+echo -n "key: "
+read SYMKEY
+export SYMKEY
+openssl aes-256-cbc -d -in $FILE_ENC -out $FILE_CLEAR -pass env:SYMKEY
+echo "Wrote out the ssl certificate to $FILE_CLEAR"
+diff $FILE_ORIGINAL $FILE_CLEAR
